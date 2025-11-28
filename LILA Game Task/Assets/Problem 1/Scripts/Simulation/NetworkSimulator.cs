fb199ab03b818a0d8448d5179d126bf00f6834eb
SanthanavelR <132562317+SanthanavelR@users.noreply.github.com>
2025-11-28 16:21:15 +0530

    Problem 1 completed

--- LILA Game Task/Assets/Problem 1/Scripts/Simulation/NetworkSimulator.cs
+++
@@ -1,9 +0,0 @@
-public static class NetworkSimulator
-{
-    public static RemotePlayerController RemotePlayer;
-
-    public static void DeliverToRemote(int x, int y, int z)
-    {
-        RemotePlayer?.ReceiveCompressedData(x, y, z);
-    }
-}
